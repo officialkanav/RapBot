acb5f1035d0d345963b06ead766f4027eb609b47
officialkanav <khajuriakanav5@gmail.com>
2021-11-03 15:14:55 +0530

    ReduceLROnPlateau added

--- model/Model.py
+++ model/Model.py
@@ -2,6 +2,8 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, CuDNNLSTM, Bidirectional
 from keras.layers import Activation
 import tensorflow as tf
+from keras.callbacks import ReduceLROnPlateau
+
 batch = 20
 
 class Model:
@@ -15,11 +17,18 @@ class Model:
 		model.add(Dense(num_classes))
 		model.add(Activation('softmax'))
 
+		reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1,
+                              patience=5, min_lr=0.000000001)
+
+		callbacks_list = [reduce_lr]
+
+		model.compile(loss='categorical_crossentropy', metrics = ['accuracy'], optimizer='RMSprop')
+
 		print('Compiling Model')
 		model.compile(loss='categorical_crossentropy', metrics = ['accuracy'], optimizer='RMSprop')
 
 		print('Training Model')
-		model.fit(X_train, y_train, batch_size=128, epochs=100,validation_data = (X_test,y_test),verbose = 1)
+		model.fit(X_train, y_train, batch_size=128, epochs=100,validation_data = (X_test,y_test),verbose = 1, callbacks = callbacks_list)
 
 		model.save_weights('my_model_weights.h5')
 		model.save('my_model.h5')
